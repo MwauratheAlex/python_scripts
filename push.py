d1aeedf8f9fada0ea5b27392b4f1ec98a64d165d
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-21 17:41:56 +0300

    initial commit

--- push.py
+++ push.py
@@ -7,6 +7,6 @@ commit_message = " ".join(s for s in sys.argv[1:])
 
 os.system("git add .")
 os.system('git commit -m "{}"'.format(commit_message))
-os.system("git push")
+os.system("git push -u origin master")
 
 print("\n\nPushed with message: {}".format(commit_message))
