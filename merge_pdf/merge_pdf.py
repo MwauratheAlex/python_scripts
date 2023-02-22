67fd2069e41e9a525e7630f2b44068a13f991fac
Mwaura <39023596+MwauratheAlex@users.noreply.github.com>
2023-02-22 12:32:37 +0300

    Update merge_pdf.py

--- merge_pdf/merge_pdf.py
+++ merge_pdf/merge_pdf.py
@@ -4,7 +4,7 @@ import os
 from PyPDF2 import PdfFileMerger
 
 # Directory containing PDF files to merge
-dir_path = os.path.join(os.path.expanduser('~'), 'work/9')
+dir_path = os.path.join(os.path.expanduser('~'), 'work/pdf_files')
 
 # Create a PdfFileMerger object
 merger = PdfFileMerger()
